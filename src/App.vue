703294d76df927611c4dc13efa350026479c79ae
iffi47 <66529774+iffi47@users.noreply.github.com>
2023-04-07 09:56:16 +0500

    Add App template

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
+  <!-- App.vue -->
+
   <v-app>
+    <v-app-bar app>
+      <!-- -->
+      <v-tabs bg-color="white">
+        <v-tab to="/">Home</v-tab>
+        <v-tab to="/camera"><v-icon>mdi-phone</v-icon>Camera</v-tab>
+      </v-tabs>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
     <v-main>
-      <router-view/>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!--  vue-router -->
+        <RouterView />
+      </v-container>
     </v-main>
+
+    <v-footer app>
+      <!-- -->
+      This is Footer
+    </v-footer>
   </v-app>
 </template>
-
-<script>
-
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-}
+<script setup>
+import { RouterView } from 'vue-router'
 </script>
+<style scoped></style>
